b2006f98e8a40212cbf1a82d970926e6680380c2
gokadroid <gokadroid@gmail.com>
2019-08-11 15:39:23 -0500

    Create bubble_sort.py

---
+++ sorting/bubble_sort.py
@@ -0,0 +1,7 @@
+def bubble_sort(arr):
+  for i in range(0,len(arr),1):
+    for j in range(0, len(arr)-1-i,1):
+      if arr[j]> arr[j+1]
+        arr[j], arr[j+1] = arr[j+1], arr[j]
+  return arr
+  
