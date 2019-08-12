38c990f8f84a1fa4ac6c413536c0ba20db586773
gokadroid <gokadroid@gmail.com>
2019-08-11 22:14:04 -0500

    Delete roman_regex.py

--- roman_regex.py
+++
@@ -1,7 +0,0 @@
-import re
-
-thousand = 'M{0,3}'
-hundred = '(C[MD]|D?C{0,3})'
-ten = '(X[CL]|L?X{0,3})'
-digit = '(I[VX]|V?I{0,3})'
-print (bool(re.match(thousand + hundred+ten+digit +'$', input())))
