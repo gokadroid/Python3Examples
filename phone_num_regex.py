fda7d487db75eb1083095a4a09cd173b65e8c03b
gokadroid <gokadroid@gmail.com>
2019-08-11 22:13:40 -0500

    Delete phone_num_regex.py

--- phone_num_regex.py
+++
@@ -1,9 +0,0 @@
-# Enter your code here. Read input from STDIN. Print output to STDOUT 789
-import re
-n = int(input())
-
-for _ in range(n):
-    if bool(re.match(r'^[789]\d{9}$', input())):
-        print("YES")
-    else:
-        print("NO")
